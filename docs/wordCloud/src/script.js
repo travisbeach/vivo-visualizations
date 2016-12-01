db0a005cf3ac8da5329dd27e3c7fea206782e45e
Travis Beach <trb96@cornell.edu>
2016-12-01 14:49:25 -0500

    organization

--- docs/wordCloud/src/script.js
+++ docs/wordCloud/src/script.js
@@ -1,8 +1,13 @@
-var words; 
-var smallerWords;
-d3.json("data/KWbyPersonCount.json", function(data){
-	words = transformUniversityWordCloud(data); 
-	smallerWords = words.filter((d, i)=>i<10);
-	drawUniversityWordCloud(words, "#viz");
+var myWordCloud;
+d3.json("data/KWbyPersonCount.json", function(data) {
 
-}); 
+    words = transformUniversityWordCloud(data); 
+    smallWords = words.filter((d, i) => i < 100); 
+
+    var currentWords = smallWords;
+    //console.log(words);
+    //Create a new instance of the word cloud visualisation.
+    myWordCloud = wordCloud('body');
+    showNewWords(myWordCloud, smallWords);
+
+});
