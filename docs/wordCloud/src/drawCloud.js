7befb93cb6563f9e80686a3fa9dd6d9b49410e6f
Travis Beach <trb96@cornell.edu>
2016-12-01 14:47:22 -0500

    Organized

--- docs/wordCloud/src/drawCloud.js
+++ docs/wordCloud/src/drawCloud.js
@@ -178,3 +178,144 @@ function updateCloud(words){
     test = d3.select("#stage").selectAll("text").data(words);
     test.exit().remove(); 
 }
+
+
+
+function wordCloud(selector) {
+
+    var fill = d3.scale.category20c();
+
+    //Construct the word cloud's SVG element
+    var svg = d3.select(selector).append("svg")
+    .attr("width", 850)
+    .attr("height", 450)
+    .append("g")
+    .attr("transform", "translate(425,225)");
+
+
+    //Draw the word cloud
+    function draw(input) {
+
+
+        var cloud = svg.selectAll("g text")
+        .data(input, function(d) { return d.keyword; })
+
+        //Entering words
+        cloud.enter()
+        .append("text")
+        .style("font-family", "Tahoma")
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr('font-size', 1)
+        .text(function(d) { return d.keyword; });
+
+        //Entering and existing words
+        cloud
+        .transition()
+        .duration(600)
+        .style("font-size", function(d) { return d.size + "px"; })
+        .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .style("fill-opacity", 1);
+
+        //Exiting words
+        cloud.exit()
+        .transition()
+        .duration(200)
+        .style('fill-opacity', 1e-6)
+        .attr('font-size', 1)
+        .remove();
+    }
+
+
+    //Use the module pattern to encapsulate the visualisation code. We'll
+    // expose only the parts that need to be public.
+    return {
+
+        //Recompute the word cloud for a new set of words. This method will
+        // asycnhronously call draw when the layout has been computed.
+        //The outside world will need to call this function, so make it part
+        // of the wordCloud return value.
+        update: function(input) {
+
+            var fontScale = makeScale(input);
+
+            d3.layout.cloud().size([800, 400])
+            .words(input, d=>d.keyword)
+            .rotate(function() { return ~~(Math.random() * 2) * 90; })
+            .font("Tahoma")
+            .fontSize(function(d) { return fontScale(d.countOfArticle); })
+            .text(d=>d.keyword)
+            .on("end", draw)
+            .start();
+        }
+    }
+
+}
+
+//Some sample data - http://en.wikiquote.org/wiki/Opening_lines
+var words = [
+"You don't know about me without you have read a book called The Adventures of Tom Sawyer but that ain't no matter.",
+"The boy with fair hair lowered himself down the last few feet of rock and began to pick his way toward the lagoon.",
+"When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.",
+"It was inevitable: the scent of bitter almonds always reminded him of the fate of unrequited love."
+]
+
+//Prepare one of the sample sentences by removing punctuation,
+// creating an array of words and computing a random size attribute.
+function getWords(i) {
+    return words[i]
+    .replace(/[!\.,:;\?]/g, '')
+    .split(' ')
+    .map(function(d) {
+        return {text: d, size: 10 + Math.random() * 60};
+    })
+}
+
+//This method tells the word cloud to redraw with a new set of words.
+//In reality the new words would probably come from a server request,
+// user input or some other source.
+function showNewWords(vis, words) {
+    vis.update(words)
+}
+
+
+d3.selectAll(".cbox").on("change", function(){
+   var checks = getChecks(); 
+   var currentWords = smallWords;
+
+   if(checks[0] == false){
+        currentWords = currentWords.filter(function(d){
+            return d.type != 'keyword'; 
+        });
+   }
+
+   if(checks[1] == false){
+        currentWords = currentWords.filter(function(d){
+            return d.type != 'mesh'; 
+        });
+    }
+    if(checks[2] == false){
+        currentWords = currentWords.filter(function(d){
+            return d.type != 'mined';
+        });
+    }
+
+   showNewWords(myWordCloud, currentWords); 
+})
+
+var checks;
+
+function getChecks(){
+   var keyword = d3.select("#keyword").property("checked"); 
+   var mesh = d3.select("#mesh").property("checked"); 
+   var mined = d3.select("#mined").property("checked"); 
+
+   return [keyword, mesh, mined]; 
+}
+
+function makeScale(array){
+    var domain = d3.extent(array, d=>d.countOfArticle); 
+    return d3.scale.linear().domain(domain).range([5, 50]);
+}
---
+++ docs/wordCloud/src/script1.js
@@ -0,0 +1,13 @@
+var myWordCloud;
+d3.json("data/KWbyPersonCount.json", function(data) {
+
+    words = transformUniversityWordCloud(data); 
+    smallWords = words.filter((d, i) => i < 100); 
+
+    var currentWords = smallWords;
+    //console.log(words);
+    //Create a new instance of the word cloud visualisation.
+    myWordCloud = wordCloud('body');
+    showNewWords(myWordCloud, smallWords);
+
+});
